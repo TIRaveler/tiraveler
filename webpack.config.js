ab17b5f89d55a7b3bc34a74e50af40544f88cee7
Linda Wang <lindawang256@gmail.com>
2018-06-23 14:50:00 -0700

    Install webpack semantic

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+ entry: './client/src/index.jsx',
+ output: {
+   path: path.resolve('client/dist'),
+   filename: 'bundle.js',
+ },
+
+ module: {
+   rules: [
+     {
+       test: /\.jsx$|\.js$/,
+       include: path.resolve('client/src'),
+       exclude: /node_modules/,
+       use: [
+         { loader: 'babel-loader' },
+       ],
+     },
+   ],
+ },
+
+ externals: {
+   react: 'React',
+   'react-dom': 'ReactDOM',
+   'semantic-ui-react': 'semanticUIReact',
+   'prop-types': 'PropTypes',
+ },  resolve: {
+   extensions: ['.js', '.jsx'],
+ },
+};
