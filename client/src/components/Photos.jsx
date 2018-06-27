c5fe4f42b117e8442ed1edae471cbab1295ef0bc
Linda Wang <lindawang256@gmail.com>
2018-06-27 16:56:04 -0700

    Add select

--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -32,8 +32,8 @@
 // export default Photos;
 
 import React, {Component} from 'react';
-import { Divider, Image } from 'semantic-ui-react'
-//import logo from './logo.svg';
+import { Divider, Image, Card } from 'semantic-ui-react'
+import { Checkbox } from 'semantic-ui-react'
 
 class Photos extends Component {
   constructor(){
@@ -44,37 +44,42 @@ class Photos extends Component {
   }
 
 componentDidMount(){
-  fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b1088ff6013a6c50850d8846e6814813&tags=nyc&per_page=10&page=1&format=json&nojsoncallback=1')
+  fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b1088ff6013a6c50850d8846e6814813&tags=San-Fransisco&per_page=20&farm=1&page=1&format=json&nojsoncallback=1')
   .then(function(response){
       return response.json();
     })
   .then(function(j){
       alert(JSON.stringify(j));
       let picArray = j.photos.photo.map((pic) => {
-
         var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+        var picTitle = pic.title;
         return(
-          <img alt="dogs" src={srcPath}></img>
+
+           <div>
+              <Image
+                  src={srcPath}
+                  text= {picTitle}
+              />
+              <Checkbox label={{ children: 'select' }} />
+           </div>
+
         )
       })
       this.setState({pictures: picArray});
+      console.log(picArray);
     }.bind(this))
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-          <h1 className="App-title">Choose 4 places you want to go</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.pictures}
-        </p>
+      <div>
+          <h1 class='ui big header'>Select 5 places you want to go! </h1>
+          <Image.Group size='medium'>
+           {this.state.pictures}
+          </Image.Group>
       </div>
-    );
+    )
   }
-
 }
 
 export default Photos;
