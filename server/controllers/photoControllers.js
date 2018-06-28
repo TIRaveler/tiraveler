6dde6d0396fc63ccd9cd410c685a11d397370cb5
zackcarlson <zackt.carlson@gmail.com>
2018-06-28 14:48:03 -0700

    Unshift price into return photo array

--- server/controllers/photoControllers.js
+++ server/controllers/photoControllers.js
@@ -16,7 +16,6 @@ const getPhotoInfo = (photoId) => {
 
 exports.search = (req, res) => {
   const location = req.body.location || '';
-
   const flickrPhotos = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API}&tags=${location}&text=${location}&per_page=10&privacy_filter=1&accuracy=16&has_geo=1&radius=5&format=json&nojsoncallback=1&authenticated=true&perms=read`;
 
   axios.get(flickrPhotos)
@@ -30,7 +29,6 @@ exports.search = (req, res) => {
       const photosByViewsCount = photosInfo.sort((photoA, photoB) => (
         photoB.photo.views - photoA.photo.views
       ));
-
       return photosByViewsCount.slice(0, 10);
     })
     .then((photosByViewsCount) => {
