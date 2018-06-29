c499e68c09ceb539dfcc0d94c9bc96f0a54d4244
JohnWebb4 <John.Webb94@gmail.com>
2018-06-29 12:14:27 -0700

    Test server returns webpages

--- server/index.test.js
+++ server/index.test.js
@@ -1,12 +1,9 @@
 import supertest from 'supertest';
-import iconv from 'iconv-lite';
-import encodings from 'iconv-lite/encodings';
-import { Sequelize } from '../db/index';
 import { app, server } from './index';
 
-iconv.encodings = encodings;
+jest.mock('../db/index');
 
-test('hello world', () => {
+test('returns index for index page', () => {
   supertest(app)
     .get('/')
     .expect(200)
@@ -16,8 +13,84 @@ test('hello world', () => {
     });
 });
 
-afterAll(() => {
-  Sequelize.close();
+test('returns index for login', () => {
+  supertest(app)
+    .get('/login')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for search', () => {
+  supertest(app)
+    .get('/search')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for time', () => {
+  supertest(app)
+    .get('/time')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for photos', () => {
+  supertest(app)
+    .get('/photos')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for events', () => {
+  supertest(app)
+    .get('/events')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for review', () => {
+  supertest(app)
+    .get('/review')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for finalized', () => {
+  supertest(app)
+    .get('/finalized')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
+});
+
+test('returns index for my itineraries', () => {
+  supertest(app)
+    .get('/myItineraries')
+    .expect(200)
+    .set('Accept', 'text/html')
+    .then((res) => {
+      expect(res.text.includes('<html>')).toEqual(true);
+    });
 });
 
 server.close();
