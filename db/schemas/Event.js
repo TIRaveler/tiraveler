913a844d7825423d66a7ef7dec6f74e0ddd5b6e5
sam-shih <samuel.jh.shih@gmail.com>
2018-06-29 19:52:54 -0700

    Save itinerary and events

--- db/schemas/Event.js
+++ db/schemas/Event.js
@@ -1,7 +1,11 @@
+const Sequelize = require('sequelize');
+
 module.exports = (database, DataTypes) => {
   const Event = database.define('event', {
     id: {
       type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      // autoIncrement: true,
       primaryKey: true,
     },
     name: DataTypes.STRING,
@@ -12,5 +16,21 @@ module.exports = (database, DataTypes) => {
     photoUrl: DataTypes.STRING,
   });
 
+  Event.saveEvent = (eventArray) => {
+    const associationPromises = [];
+    const eventIds = [];
+    eventArray.forEach((event) => {
+      associationPromises.push(Event.create({
+        name: event.name,
+        location: event.location,
+        yelplink: event.yelplink,
+        tags: event.tags,
+        price: event.price,
+        photoUrl: event.photoUrl,
+      }).then(result => result.dataValues.id));
+      return Sequelize.Promise.all(associationPromises);
+    });
+    console.log(associationPromises, 'MORE GOODS');
+  };
   return Event;
 };
