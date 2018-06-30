63202a5566e4c52ef3398f046875738ff6b15cd4
Linda Wang <lindawang256@gmail.com>
2018-06-30 15:34:29 -0700

    Fix bug in url

--- server/controllers/photoControllers.js
+++ server/controllers/photoControllers.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 
 const getPhotoInfo = (photoId) => {
-  const flickrPhotoInfo = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo
-  &api_key=${process.env.FLICKR_API}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+  const flickrPhotoInfo = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${process.env.FLICKR_API}&photo_id=${photoId}&format=json&nojsoncallback=1`;
 
   return (axios.get(flickrPhotoInfo)
     .then((data) => {
