b40b7a28cb90b581e73ca96fa75e08df5c3b0618
JohnWebb4 <John.Webb94@gmail.com>
2018-07-02 19:38:49 -0700

    Use make logger for stream to consolidate tests

--- server/Console.test.js
+++ server/Console.test.js
@@ -4,47 +4,42 @@ import Console from './Console';
 
 describe('Console', () => {
   let consoleLog = [];
-  let errorLog = [];
   const oldWriteLog = process.stdout.write;
-  const oldWriteError = process.stderr.write;
+  // Console.log, Console.error, and Console.Console classes methods are instances
+  // of makeLogger for Stream by testing this you are effectively testing everything
+  const logToStream = Console.makeLoggerForStream(process.stdout);
 
   const stubLog = stub(process.stdout, 'write').callsFake((...args) => {
     args.forEach(arg => consoleLog.push(arg));
     oldWriteLog(...args);
   });
 
-  const stubError = stub(process.stderr, 'write').callsFake((...args) => {
-    args.forEach(arg => errorLog.push(arg));
-    oldWriteError(...args);
-  });
-
   beforeEach(() => {
     consoleLog = [];
-    errorLog = [];
 
-    process.stderr.write = stubError;
     process.stdout.write = stubLog;
   });
 
   afterEach(() => {
-    process.stderr.write.restore();
     process.stdout.write.restore();
   });
 
-  test('has log and error', () => {
+  test('has log, error, makeLoggerForStream, and Console', () => {
     expect(Console.log).toBeDefined();
     expect(Console.error).toBeDefined();
+    expect(Console.makeLoggerForStream).toBeDefined();
+    expect(Console.Console).toBeDefined();
   });
 
   test('can log', () => {
     const testString = 'Test';
 
-    Console.log(testString);
+    logToStream(testString);
 
     expect(consoleLog[0]).toEqual(`${testString}\n`);
   });
 
-  test('can log multiple', () => {
+  test('can log multiple arguments', () => {
     const testStrings = [
       'This',
       'Is',
@@ -54,31 +49,28 @@ describe('Console', () => {
 
     const stringResult = 'This Is A Test\n';
 
-    Console.log(...testStrings);
+    logToStream(...testStrings);
 
     expect(consoleLog[0]).toEqual(stringResult);
   });
 
-  test('can write errors', () => {
-    const testString = 'Test';
+  test('can log any data type', () => {
+    logToStream('Test');
+    expect(consoleLog[0]).toEqual('Test\n');
 
-    Console.error(testString);
+    logToStream(1);
+    expect(consoleLog[1]).toEqual('1\n');
 
-    expect(errorLog[0]).toEqual(`${testString}\n`);
-  });
+    logToStream(undefined);
+    expect(consoleLog[2]).toEqual('undefined\n');
 
-  test('can write multiple errors', () => {
-    const testStrings = [
-      'This',
-      'Is',
-      'An',
-      'Error',
-    ];
+    logToStream(null);
+    expect(consoleLog[3]).toEqual('null\n');
 
-    const stringResult = 'This Is An Error\n';
+    logToStream(['This', 'is', 'a', 'test']);
+    expect(consoleLog[4]).toEqual('[\nThis\nis\na\ntest\n]');
 
-    Console.error(...testStrings);
-
-    expect(errorLog[0]).toEqual(stringResult);
+    logToStream({ test: 'This', is: 'one' });
+    expect(consoleLog[5]).toEqual('{\ntest: \'This\'\nis: \'one\'');
   });
 });
