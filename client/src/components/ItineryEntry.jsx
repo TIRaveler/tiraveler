63ac1467ca304d6658066dc1f722d70447fff1f3
sam-shih <samuel.jh.shih@gmail.com>
2018-07-02 18:37:14 -0700

    Prepare Review form to list entries

---
+++ client/src/components/ItineryEntry.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ItineraryEntry = ({ entry }) => (
+
+  <div className="entry-list-entry media">
+    <div className="media-left media-middle">
+      <img className="media-object" src={entry.snippet.thumbnails.default.url} alt="" />
+    </div>
+    <div className="media-body">
+      <div className="entry-list-entry-title">{entry.snippet.title}</div>
+      <div className="entry-list-entry-detail">{entry.snippet.description}</div>
+    </div>
+  </div>
+);
+
+
+export default ItineraryEntry;
