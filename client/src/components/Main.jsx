78d34e440e816f6b4861c73d3fb491d0d42bf719
zackcarlson <zackt.carlson@gmail.com>
2018-07-02 21:05:22 -0700

    Pass down twitter oauth function

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -6,14 +6,10 @@ import StatCard from './StatCard';
 import AboutUs from './AboutUs';
 import GetStarted from './GetStarted';
 
-const signInBtn = (
-  <Button primary size="massive">
-Get Started
-  </Button>
-);
-const Main = () => (
+const signInBtn = <Button primary size="massive">Get Started</Button>;
+const Main = ({ isAuthenticated, twitterLogin }) => (
   <div>
-    <Navbar />
+    <Navbar isAuthenticated={isAuthenticated} twitterLogin={twitterLogin} />
     <Parallax
       bgImage="https://image.ibb.co/nN3bjy/adult_book_business_297755_1.jpg"
       bgImageAlt="welcome"
@@ -21,7 +17,7 @@ const Main = () => (
     >
       <div style={{ height: '550px' }} />
       <div className="ui center aligned segment" style={{ background: 'transparent', border: 'none', paddingBottom: '70px' }}>
-        <GetStarted signin={signInBtn} />
+        <GetStarted signin={signInBtn} twitterLogin={twitterLogin} />
       </div>
     </Parallax>
     <Parallax
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import GetStarted from './GetStarted.jsx';
 
-const signinBtn = (
-  <button type="button" className="ui basic button">
-Sign In
-  </button>
-);
-const Navbar = () => (
+const signinBtn = <button type="button" className="ui basic button">Sign In</button>;
+const Navbar = ({ twitterLogin }) => (
+
   <div className="ui top fixed menu secondary pointing menu" style={{ background: 'white' }}>
     <div className="item">
       <img className="ui small image" src="https://image.ibb.co/hYsNrd/Screen_Shot_2018_06_29_at_6_06_38_PM.png" alt="logo" />
     </div>
     <div className="right menu">
       <div className="item" style={{ marginBottom: '10px' }}>
-        <GetStarted signin={signinBtn} />
+        <GetStarted signin={signinBtn} twitterLogin={twitterLogin} />
       </div>
     </div>
   </div>
