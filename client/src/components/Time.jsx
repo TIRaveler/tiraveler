acc556984b3340c72295cae23cf42a57c2f531a6
sam-shih <samuel.jh.shih@gmail.com>
2018-07-05 10:15:00 -0700

    Add time selection page

--- client/src/components/Time.jsx
+++ client/src/components/Time.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Segment } from 'semantic-ui-react';
 
 const Time = () => (
   <div>
-Time
+    <Segment padded>
+      <Segment.Group>
+        <Segment textAlign="center" size="massive">
+          Time
+        </Segment>
+        <Segment.Group horizontal>
+          <Segment textAlign="center" size="massive">
+            Spring
+          </Segment>
+          <Segment textAlign="center" size="massive">
+            Summer
+          </Segment>
+          <Segment textAlign="center" size="massive">
+            Fall
+          </Segment>
+          <Segment textAlign="center" size="massive">
+            Winter
+          </Segment>
+          <Segment textAlign="center" size="massive">
+            Any
+          </Segment>
+        </Segment.Group>
+      </Segment.Group>
+    </Segment>
   </div>
 );
 
