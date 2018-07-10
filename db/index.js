56061c7aa16f22e16f0c7d9094fd042ea3121dc4
Linda Wang <lindawang256@gmail.com>
2018-07-09 21:39:37 -0700

    Change to local DB server

--- db/index.js
+++ db/index.js
@@ -1,25 +1,25 @@
 const Sequelize = require('sequelize');
 const mysql = require('mysql2');
 
-const databaseUrl = process.env.DATABASE_URL
-  || `mysql://${process.env.DB_USER}${process.env.DB_PASSWORD ? `:${process.env.DB_PASSWORD}` : undefined}@localhost/TIRavelerDB?reconnect=true`;
+// const databaseUrl = process.env.DATABASE_URL
+//   || `mysql://${process.env.DB_USER}${process.env.DB_PASSWORD ? `:${process.env.DB_PASSWORD}` : undefined}@localhost/TIRavelerDB?reconnect=true`;
 
-const database = new Sequelize(databaseUrl, {
-  dialect: 'mysql',
-});
-
-
-const db = mysql.createConnection(databaseUrl);
-// const database = new Sequelize('TIRavelerDB', 'root', process.env.DB_PASSWORD, {
+// const database = new Sequelize(databaseUrl, {
 //   dialect: 'mysql',
 // });
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   password: process.env.DB_PASSWORD,
-//   database: 'TIRavelerDB',
-//   user: 'root', // default
-// });
+
+//const db = mysql.createConnection(databaseUrl);
+const database = new Sequelize('TIRavelerDB', 'root', process.env.DB_PASSWORD, {
+  dialect: 'mysql',
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  password: process.env.DB_PASSWORD,
+  database: 'TIRavelerDB',
+  user: 'root', // default
+});
 
 const Event = require('./schemas/Event')(database, Sequelize);
 const Itinerary = require('./schemas/Itinerary')(database, Sequelize);
