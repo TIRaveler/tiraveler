6e1f0ddb4d6d59afbb517bac617baee71a8b977a
JohnWebb4 <John.Webb94@gmail.com>
2018-07-09 20:33:06 -0700

    Format itinerary data, and add sample data

---
+++ server/controllers/sample_data/itineraries.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    events: [
+      {
+        address: '55 N La Cienega Blvd,Beverly Hills, CA 90211',
+        eventName: 'The Stinking Rose',
+        id: '3db578dc-feea-4b63-84b4-4a04b8815d8d',
+        itinerariesName: 'Los Angles trip',
+        price: '$$',
+      },
+      {
+        address:
+        '7916 W Sunset Blvd,Hollywood, CA 90046',
+        eventName: 'The Griddle Cafe',
+        id: '3db578dc-feea-4b63-84b4-4a04b8815d8d',
+        itinerariesName: 'Los Angles trip',
+        price: '$$',
+      },
+      {
+        address: 'Los Angeles, CA 90015',
+        eventName: 'Six Taste',
+        id: '3db578dc-feea-4b63-84b4-4a04b8815d8d',
+        itinerariesName: 'Los Angles trip',
+        price: null,
+      },
+    ],
+    name: 'Los Angles trip',
+  },
+];
