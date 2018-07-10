ba7ebcdbc9e57e627005e721c8dda392a7157d38
JohnWebb4 <John.Webb94@gmail.com>
2018-07-10 14:05:37 -0700

    Nav bar sticks to top if not logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = ({
   popUpMessage,
   setItineraries,
 }) => (
-  <div className="ui top fixed menu secondary pointing menu" style={{ background: 'white' }}>
+  <div className={`ui ${name ? '' : 'top fixed'} secondary pointing menu`} style={{ background: 'white' }}>
     <div className="item">
       <img className="ui small image" src="https://image.ibb.co/hYsNrd/Screen_Shot_2018_06_29_at_6_06_38_PM.png" alt="logo" />
     </div>
