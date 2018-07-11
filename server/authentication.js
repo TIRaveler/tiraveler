be914d899a8bafdc905be459a863a63bd88d5c56
JohnWebb4 <John.Webb94@gmail.com>
2018-07-10 17:45:19 -0700

    Hide OAuth authentication if not used

--- server/authentication.js
+++ server/authentication.js
@@ -1,20 +1,20 @@
-const passport = require('passport');
-const { Strategy } = require('passport-twitter');
+// const passport = require('passport');
+// const { Strategy } = require('passport-twitter');
 
-passport.use(new Strategy({
-  consumerKey: process.env.TWITTER_API,
-  consumerSecret: process.env.TWITTER_SECRET,
-  callbackURL: 'http://127.0.0.1:8000/auth/twitter/callback',
-}, (token, tokenSecret, profile, done) => done(null, profile)));
+// passport.use(new Strategy({
+//   consumerKey: process.env.TWITTER_API,
+//   consumerSecret: process.env.TWITTER_SECRET,
+//   callbackURL: 'http://127.0.0.1:8000/auth/twitter/callback',
+// }, (token, tokenSecret, profile, done) => done(null, profile)));
 
-passport.serializeUser((user, done) => {
-  // console.log('serialize user in server: ', user)
-  done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//   // console.log('serialize user in server: ', user)
+//   done(null, user);
+// });
 
-passport.deserializeUser((obj, done) => {
-  // console.log('deserialize obj in server: ', obj)
-  done(null, obj);
-});
+// passport.deserializeUser((obj, done) => {
+//   // console.log('deserialize obj in server: ', obj)
+//   done(null, obj);
+// });
 
-module.exports = passport;
+// module.exports = passport;
