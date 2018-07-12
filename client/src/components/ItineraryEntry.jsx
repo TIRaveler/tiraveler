31b311eba9179aec2a97083988820a8cf31ba0d2
Linda Wang <lindawang256@gmail.com>
2018-07-11 20:01:23 -0700

    Delete old format of review page

--- client/src/components/ItineraryEntry.jsx
+++ client/src/components/ItineraryEntry.jsx
@@ -37,31 +37,3 @@ ItineraryEntry.propTypes = {
 };
 
 export default ItineraryEntry;
-
-
-
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// const ItineraryEntry = ({ event }) => (
-//   <div className="entry-list-entry media">
-//     <div className="media-left media-middle">
-//       <img className="media-object" style={{width: '360px', height: '360px'}}src={event.image_url} alt="" />
-//     </div>
-//     <div className="media-body">
-//       <div className="entry-list-entry-title">
-//         {event.eventName}
-//       </div>
-//     </div>
-//   </div>
-// );
-
-// ItineraryEntry.propTypes = {
-//   event: PropTypes.shape({
-//     image_url: PropTypes.string,
-//     name: PropTypes.string,
-//   }).isRequired,
-// };
-
-// export default ItineraryEntry;
