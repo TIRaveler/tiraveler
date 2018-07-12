141aed780b003435082c2c7a1481890f7a677fd0
Linda Wang <lindawang256@gmail.com>
2018-07-12 13:30:28 -0700

    Add background photo to Itinerary.jsx

--- client/src/components/Itinerary.jsx
+++ client/src/components/Itinerary.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { Container, Header, Item } from 'semantic-ui-react';
 import ItineraryModalEntry from './ItineraryModalEntry';
 
-const Itinerary = ({ entries }) => (
-  <div>
+ const Itinerary = ({ entries }) => (
+  <div style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1515888368305-5a7eba31b4e3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=e715091889b2723e452d5b8fc860e4db")',height: '800px'}}>
     <Header as="h1" textAlign="center">
-    Your Itineraries
+    My Itineraries
     </Header>
     <Container text>
       <Item.Group divided>
@@ -17,6 +17,7 @@ const Itinerary = ({ entries }) => (
     </Container>
   </div>
 );
+
 
 Itinerary.propTypes = {
   entries: PropTypes.arrayOf(PropTypes.shape({
