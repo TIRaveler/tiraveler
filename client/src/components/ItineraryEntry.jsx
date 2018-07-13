7839f4fe63861fce7b5c7662001689b0cdea2851
sam-shih <samuel.jh.shih@gmail.com>
2018-07-13 00:37:04 -0700

    Remove divider from Review page

--- client/src/components/ItineraryEntry.jsx
+++ client/src/components/ItineraryEntry.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Image,
-  Card,
-} from 'semantic-ui-react';
+import { Item } from 'semantic-ui-react';
 
 const ItineraryEntry = ({ event }) => (
-  <div className="entry-list-entry media">
-    <div className="media-left media-middle">
-      <Card>
-          <Image style={{ width: '300px',height: '300px' }} center src={event.image_url} />
-          <Card.Content>
-            <Card.Header>
-              {event.name}
-            </Card.Header>
-            <Card.Description>
-              <p>Price: {event.price ? event.price : "Free or N/A"}</p>
-              <p>Rating: {event.rating}</p>
-              <p> {event.address ? event.address : " "} </p>
-            </Card.Description>
-          </Card.Content>
-      </Card>
-    </div>
-    <div className="media-body">
-      <div className="entry-list-entry-title">
-        {event.eventName}
-      </div>
-    </div>
-  </div>
+  <Item>
+    <Item.Image style={{ height: '180px', width: '200px' }} src={event.image_url} />
+    <Item.Content>
+      <Item.Header as="a">
+        {event.name}
+      </Item.Header>
+      <Item.Description>
+        <p>
+          Price: {event.price ? event.price : "Free or N/A"}
+        </p>
+        <p>
+          Rating: {event.rating}
+        </p>
+        <p>
+          {event.address ? event.address : " "}
+        </p>
+      </Item.Description>
+    </Item.Content>
+  </Item>
 );
 
 ItineraryEntry.propTypes = {
--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Button, Modal } from 'semantic-ui-react';
+import { Button, Modal, Item } from 'semantic-ui-react';
 import ItineraryEntry from './ItineraryEntry';
 
 
@@ -89,7 +89,7 @@ class Review extends React.Component {
         )}
         open={isOpen}
         onClose={this.close}
-        basic
+        // basic
       >
         <Modal.Header>
           Please Review Your Epic Itinerary
@@ -102,7 +102,7 @@ class Review extends React.Component {
             }
               floated="right"
             >
-              FINALIZE!
+              SUBMIT ITINERARY!
             </Button>
           )}
           />
@@ -111,11 +111,11 @@ class Review extends React.Component {
           </Button>
         </Modal.Header>
         <Modal.Content scrolling>
-          <div>
+          <Item.Group>
             {
               entries.map(entry => <ItineraryEntry key={entry.name} event={entry} />)
             }
-          </div>
+          </Item.Group>
         </Modal.Content>
       </Modal>
     );
