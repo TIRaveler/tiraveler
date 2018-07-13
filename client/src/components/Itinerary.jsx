b92702cab5ec7169d35b93e67ed084e79da1bb57
Linda Wang <lindawang256@gmail.com>
2018-07-12 19:00:34 -0700

    Add picture to search.jsx

--- client/src/components/Itinerary.jsx
+++ client/src/components/Itinerary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Header, Item } from 'semantic-ui-react';
+import { Parallax } from 'react-parallax';
 import Axios from 'axios';
 
 import ItineraryModalEntry from './ItineraryModalEntry';
@@ -31,7 +32,33 @@ class Itinerary extends React.Component {
       </div>
     );
   }
+
+
+  // render() {
+  //   const { entries } = this.props;
+  //   return (
+  //     <div>
+  //      <Parallax
+  //         blur={{ min: -15, max: 5 }}
+  //         bgImage="https://images.unsplash.com/photo-1515888368305-5a7eba31b4e3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=e715091889b2723e452d5b8fc860e4db"
+  //         strength={200}>
+
+  //       <Header as="h1" textAlign="center">
+  //        My Itineraries
+  //       </Header>
+  //       <Container text>
+  //         <Item.Group divided>
+  //           {
+  //             entries.map(entry => <ItineraryModalEntry key={entry.name} entry={entry} />)
+  //           }
+  //         </Item.Group>
+  //       </Container>
+  //      </Parallax>
+  //     </div>
+  //   );
+  // }
 }
+
 
 
 Itinerary.propTypes = {
